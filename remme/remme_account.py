cb5889e4184b832634d51acc56671fdb2e771f47
dethline <dethline@te.net.ua>
2018-10-17 22:16:19 +0300

    remme account

--- remme/remme_account.py
+++ remme/remme_account.py
@@ -1,18 +1,40 @@
-from remme.remme_utils import hex_to_bytes
+from sawtooth_signing.secp256k1 import Secp256k1PrivateKey
 import sawtooth_signing
+from remme.remme_utils import hex_to_bytes
+from remme.remme_functions import get_address_from_data
+import re
 
 
 class RemmeAccount:
 
+    _family_name = None
     _signer = None
     private_hex_key = None
     public_hex_key = None
     address = None
 
-    def __init__(self, private_hex):
-        pass
+    def __init__(self, private_key_hex):
+        self._family_name = "account"
+        if private_key_hex and not self.is_valid_private_hex(private_key_hex):
+            raise Exception("Invalid private key given!")
+        context = sawtooth_signing.create_context("secp256k1")
+        if not private_key_hex:
+            private_key = context.new_random_private_key()
+        else:
+            private_key = Secp256k1PrivateKey.from_hex(private_key_hex)
+        self._signer = sawtooth_signing.CryptoFactory(context).new_signer(private_key)
+        self.private_hex_key = private_key.as_hex()
+        self.public_hex_key = self._signer.get_public_key().as_hex()
+        self.address = get_address_from_data(self._family_name, self.public_hex_key)
+
+    @staticmethod
+    def is_valid_private_hex(_private_hex):
+        return re.match(r"^[0-9a-f]{64}$", _private_hex) is not None
 
     def sign(self, transaction):
         if type(transaction) == str:
             transaction = hex_to_bytes(transaction)
         return self._signer.sign(transaction)
+
+    def get_private_key(self):
+        return Secp256k1PrivateKey.from_hex(self.private_hex_key)
---
+++ remme/remme_functions.py
@@ -0,0 +1,31 @@
+
+def hex_to_bytes(_string):
+    raise NotImplementedError
+
+
+def bytes_to_hex(_uint8arr):
+    raise NotImplementedError
+
+
+def base64_to_array_buffer(_base64):
+    raise NotImplementedError
+
+
+def utf8_to_bytes(_string):
+    raise NotImplementedError
+
+
+def to_hex(_string):
+    raise NotImplementedError
+
+
+def get_address_from_data(_family_name, _data):
+    raise NotImplementedError
+
+
+def to_hex_string(_byte_array):
+    raise NotImplementedError
+
+
+def to_utf8_array(_string):
+    raise NotImplementedError
