0b9b37462264e8881d8d1b1eebc8e2b952d7cc0b
dethline <dethline@te.net.ua>
2018-10-20 14:45:26 +0300

    remme functions get_address_from_data

--- remme/remme_functions.py
+++ remme/remme_functions.py
@@ -1,3 +1,5 @@
+import hashlib
+
 
 def hex_to_bytes(_string):
     raise NotImplementedError
@@ -20,7 +22,14 @@ def to_hex(_string):
 
 
 def get_address_from_data(_family_name, _data):
-    raise NotImplementedError
+    print(f"get_address_from_data args: {_family_name}  ,  {_data}")
+    _family_name = _family_name if isinstance(_family_name, bytes) else _family_name.encode('UTF-8')
+    _data = _data if isinstance(_data, bytes) else _data.encode('UTF-8')
+    family_name_o = hashlib.sha512(_family_name)
+    data_o = hashlib.sha512(_data)
+    result = "" + family_name_o.hexdigest()[slice(0, 6)] + data_o.hexdigest()[slice(0, 64)]
+    print(f"get_address_from_data result: {result}")
+    return result
 
 
 def to_hex_string(_byte_array):
