e30af412a80bbf0276434a7a2d923f4de83f054c
dethline <dethline@te.net.ua>
2018-10-31 17:54:30 +0300

    remme account

--- remme/remme_websocket.py
+++ remme/remme_websocket.py
@@ -12,36 +12,36 @@ class RemmeWebSocket:
     @example
     ```python
     remme = new Remme()
-    some_remme_address = "03c2e53acce583c8bb2382319f4dee3e816b67f3a733ef90fe3329062251d0c638";
-    transaction_result = await remme.token.transfer(some_remme_address, 10);
+    some_remme_address = "03c2e53acce583c8bb2382319f4dee3e816b67f3a733ef90fe3329062251d0c638"
+    transaction_result = await remme.token.transfer(some_remme_address, 10)
 
     # transaction_result is inherit from RemmeWebSocket and self.data = {
     #        batch_ids: [
     #           transactionResult.batchId,
     #        ],
-    #    };
+    #    }
     # so you can connect_to_web_socket easy. Just:
 
-    transactionResult.connectToWebSocket((err: Error, res: any) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(res);
-        mySocketConnection.closeConnection();
-    });
+    def callback(err, res):
+        if err:
+            print(err)
+            return
+        print(res)
+        my_socket_connection.close_connection()
+
+    transactionResult.connect_to_web_socket(callback)
     ```
 
     But you also can use your class for work with WebSockets. Just inherit it from RemmeWebSocket, like this:
     ```python
     class MySocketConnection(RemmeWebSocket) {
          def __init__({node_address: node_address, ssl_mode: ssl_mode, data: data}) {
-             super(node_address, ssl_mode);
-             self.data = data;
+             super(node_address, ssl_mode)
+             self.data = data
          }
     }
 
-    remme_web_socket = mySocketConnection({
+    my_socket_connection = MySocketConnection({
          node_address: "localhost:8080",
          ssl_mode: False,
          data: {
@@ -49,28 +49,37 @@ class RemmeWebSocket:
                 transaction_result.batch_id,
              ],
          }
-    });
+    })
 
-    mySocketConnection.connectToWebSocket((err: Error, res: any) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(res);
-        mySocketConnection.closeConnection();
-    });
+    def callback(err, res):
+        if err:
+            print(err)
+            return
+        print(res)
+        my_socket_connection.close_connection()
+
+    my_socket_connection.connect_to_web_socket(callback)
     ```
     """
 
     _is_event = None
-    _socket_address = None
+    _node_address = None
     _ssl_mode = None
     _data = None
     _socket = None
 
-    def __init__(self, socket_address, ssl_mode):
+    def __init__(self, node_address, ssl_mode):
+        """
+        Implement RemmeWebSocket by providing node address and ssl mode.
+        @example
+        ```python
+        remme_web_socket = RemmeWebSocket(node_address, ssl_mode)
+        ```
+        :param node_address: {string}
+        :param ssl_mode: {boolean}
+        """
         self._is_event = False
-        self._socket_address = socket_address
+        self._node_address = node_address
         self._ssl_mode = ssl_mode
 
     async def connect_to_websocket(self, callback):
@@ -85,7 +94,7 @@ class RemmeWebSocket:
     def _get_subscribe_url(self):
         protocol = "wss://" if self._ssl_mode else "ws://"
         events = "/events" if self._is_event else ""
-        return protocol + self._socket_address + '/ws' + events
+        return protocol + self._node_address + '/ws' + events
 
     def _get_socket_query(self, is_subscribe=True):
         if not self._data:
@@ -115,7 +124,7 @@ class RemmeWebSocket:
 
     @property
     def socket_address(self):
-        return self._socket_address
+        return self._node_address
 
     @property
     def ssl_mode(self):
