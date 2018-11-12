7c5b8e79e84876102247deb369a7621127a9d022
dethline <dethline@te.net.ua>
2018-11-12 16:21:23 +0300

    REM-776

--- example_pubkey.py
+++ example_pubkey.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 from remme.remme import Remme
 import asyncio
@@ -8,9 +9,9 @@ from datetime import datetime, timedelta
 
 async def example():
     private_key_hex = "7f752a99bbaf6755dc861bb4a7bb19acb913948d75f3b718ff4545d01d9d4ff5"
-    private_key_file = open("test_rsa_private.key", "rb")
-    public_key_file = open("test_rsa_public.key", "rb")
     remme = Remme(private_key_hex=private_key_hex)
+    # private_key_file = open("test_rsa_private.key", "rb")
+    # public_key_file = open("test_rsa_public.key", "rb")
     # data = {
     #     "data": "store data",
     #     "private_key": private_key_file.read(),
@@ -18,7 +19,7 @@ async def example():
     #     "valid_from": int(datetime.utcnow().strftime("%s")),
     #     "valid_to": int((datetime.utcnow() + timedelta(days=365)).strftime("%s"))
     # }
-    private_key = serialization.load_pem_private_key(private_key_file.read(), password=None, backend=default_backend())
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
     data = {
         "data": "store data",
         "private_key": private_key,
@@ -27,8 +28,17 @@ async def example():
         "valid_to": int((datetime.utcnow() + timedelta(days=365)).strftime("%s"))
     }
     pubkey_storage_transaction_result = await remme.public_key_storage.store(**data)
+
+    batch_status = await remme.batch.get_status(pubkey_storage_transaction_result.batch_id)
+    print(f"batch status {batch_status}\n")
+
     async for response in pubkey_storage_transaction_result.connect_to_web_socket():
         print("connected")
+        batch_status = await remme.batch.get_status(pubkey_storage_transaction_result.batch_id)
+        print(f"batch status {batch_status}\n")
+        # if response.status == BatchStatus.COMMITTED.value:
+        #     batch_status = await remme.batch.get_status(pubkey_storage_transaction_result.batch_id)
+        #     print(f"batch status {batch_status}\n")
         await pubkey_storage_transaction_result.close_web_socket()
 
 
