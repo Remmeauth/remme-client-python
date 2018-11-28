6676db728e9a35a5e3dffea2a3437a375f6aa6ed
Anastasia Bilova <anastasiabilova@gmail.com>
2018-11-28 13:06:03 +0200

    Create validation for address, transfer by address instead of public key

--- remme/remme_token.py
+++ remme/remme_token.py
@@ -1,4 +1,3 @@
-from remme.remme_utils import generate_address
 from remme.constants.remme_family_name import RemmeFamilyName
 from remme.constants.remme_methods import RemmeMethods
 from remme.protos.account_pb2 import AccountMethod, TransferPayload
@@ -20,25 +19,29 @@ class RemmeToken:
             raise Exception("Invalid amount")
         return amount
 
-    async def _create_transfer_tx(self, public_key_to, amount):
-        public_key_to = self.validate_public_key(public_key_to)
+    @staticmethod
+    def validate_public_key(key):
+        if len(key) != 66:
+            raise Exception("Invalid key")
+        return key
+
+    @staticmethod
+    def validate_public_key_address(address):
+        if len(address) != 70:
+            raise Exception("Invalid address")
+        return address
+
+    async def _create_transfer_tx(self, address, amount):
+        receiver_address = self.validate_public_key_address(address=address)
         amount = self.validate_amount(amount)
-        receiver_address = generate_address(RemmeFamilyName.ACCOUNT.value, public_key_to)
-        # print(f"receiver address: {receiver_address}")
 
         transfer = TransferPayload()
         transfer.address_to = receiver_address
         transfer.value = amount
 
-        # print(f"transfer address : {transfer.address_to} ; transfer value : {transfer.value}")
-        # print(f"transfer serialized : {transfer.SerializeToString()}")
-
         tr = TransactionPayload()
         tr.method = AccountMethod.TRANSFER
         tr.data = transfer.SerializeToString()
-
-        # print(f"transaction method : {tr.method} ; transaction data : {tr.data}")
-        # print(f"transaction serialized : {tr.SerializeToString()}")
 
         return await self.transaction_service.create(
             family_name=RemmeFamilyName.ACCOUNT.value,
@@ -48,18 +51,13 @@ class RemmeToken:
             payload_bytes=tr.SerializeToString(),
         )
 
-    async def transfer(self, public_key_to, amount):
-        payload = await self._create_transfer_tx(public_key_to, amount)
+    async def transfer(self, receiver_address, amount):
+        receiver_address = self.validate_public_key_address(address=receiver_address)
+        payload = await self._create_transfer_tx(receiver_address, amount)
         return await self.transaction_service.send(payload)
 
-    @staticmethod
-    def validate_public_key(key):
-        if len(key) != 66:
-            raise Exception("Invalid key")
-        return key
-
     async def get_balance(self, public_key_address):
-        params = {'public_key_address': public_key_address}
+        address = self.validate_public_key_address(address=public_key_address)
+        params = {'public_key_address': address}
         result = await self.api.send_request(RemmeMethods.TOKEN, params)
-        # print(f'get_balance result: {result}')
         return result
