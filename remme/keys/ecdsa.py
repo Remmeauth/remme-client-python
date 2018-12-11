eeb1f37a3886c09633d1a040456409747e79f670
Anastasia Bilova <anastasiabilova@gmail.com>
2018-12-11 16:24:00 +0200

    REM-857: refactor ECDSA class, make the key object instead of the string format

--- remme/keys/ecdsa.py
+++ remme/keys/ecdsa.py
@@ -2,10 +2,7 @@ import base64
 
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import (
-    hashes,
-    serialization,
-)
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import (
     ec,
     utils,
@@ -18,12 +15,17 @@ from remme.models.key_dto import KeyDto
 from remme.remme_utils import (
     dict_to_base64,
     generate_address,
+    private_key_to_pem,
+    public_key_to_pem,
 )
 
 
 class ECDSA(KeyDto, IRemmeKeys):
     """
     ECDSA (secp256k1) class implementation.
+
+    References:
+        - https://cryptography.io/en/latest/hazmat/primitives/asymmetric/ec/
     """
 
     def __init__(self, private_key, public_key):
@@ -40,20 +42,10 @@ class ECDSA(KeyDto, IRemmeKeys):
 
         elif private_key:
             self._private_key = private_key
-            private_key_obj = serialization.load_pem_private_key(
-                self._private_key,
-                password=None,
-                backend=default_backend(),
-            )
-            public_key_obj = private_key_obj.public_key()
+            self._public_key = self._private_key.public_key()
 
-            self._public_key = public_key_obj.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo,
-            )
-
-        self._private_key_hex = self.private_key.encode('hex')
-        self._public_key_hex = self._public_key.encode('hex')
+        self._private_key_hex = private_key_to_pem(self.private_key).encode('hex')
+        self._public_key_hex = public_key_to_pem(self._public_key).encode('hex')
 
         self._public_key_base64 = dict_to_base64(self._public_key_hex)
         self._address = generate_address(RemmeFamilyName.PUBLIC_KEY, self._public_key_base64)
@@ -65,13 +57,10 @@ class ECDSA(KeyDto, IRemmeKeys):
         Generate public and private key pair.
         :return: generated key pair
         """
-        private_key = ec.generate_private_key(
+        return ec.generate_private_key(
             curve=ec.SECP256K1,
             backend=default_backend(),
         )
-        public_key = private_key.public_key()
-
-        return private_key, public_key
 
     @staticmethod
     def get_address_from_public_key(public_key):
@@ -80,7 +69,7 @@ class ECDSA(KeyDto, IRemmeKeys):
         :param public_key
         :return: address in blockchain generated from public key string
         """
-        public_key_hex = public_key.encode('hex')
+        public_key_hex = public_key_to_pem(public_key).encode('hex')
         public_key_base64 = dict_to_base64(public_key_hex)
         return generate_address(RemmeFamilyName.PUBLIC_KEY, public_key_base64)
 
@@ -90,18 +79,12 @@ class ECDSA(KeyDto, IRemmeKeys):
         :param data: data string which will be signed
         :return: hex string for signature
         """
-        private_key = serialization.load_pem_private_key(
-            data=self._private_key,
-            password=None,
-            backend=default_backend(),
-        )
-
         chosen_hash = hashes.SHA256()
         hasher = hashes.Hash(chosen_hash, default_backend())
         hasher.update(data.encode('utf-8'))
         digest = hasher.finalize()
 
-        signature = base64.b64encode(private_key.sign(
+        signature = base64.b64encode(self._private_key.sign(
             digest,
             ec.ECDSA(utils.Prehashed(chosen_hash)),
         ))
@@ -115,18 +98,13 @@ class ECDSA(KeyDto, IRemmeKeys):
         :param signature: hex string of signature
         :return: none: in case signature is correct
         """
-        public_key = serialization.load_pem_public_key(
-            self._public_key,
-            backend=default_backend(),
-        )
-
         try:
             chosen_hash = hashes.SHA256()
             hasher = hashes.Hash(chosen_hash, default_backend())
             hasher.update(data.encode('utf-8'))
             digest = hasher.finalize()
 
-            signature_verified = public_key.verify(
+            signature_verified = self._public_key.verify(
                 base64.b64decode(signature),
                 digest,
                 ec.ECDSA(utils.Prehashed(chosen_hash)),
